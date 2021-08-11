62013e29e451e23bedbab53146c2697cd67dae47
Devdreamsolution <q3construction1@gmail.com>
2021-08-11 16:36:27 +0900

    Add nav link

--- src/components/common/navbar/index.js
+++ src/components/common/navbar/index.js
@@ -3,28 +3,29 @@ import { Link } from 'react-router-dom'
 
 import "./navbar.css"
 
-export default function NavBar() {
+export default function NavBar(props) {
 	return (
 		<Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="dark"
-        variant="dark"
-        sticky="top"
-        >
-        <Navbar.Brand href="/"><img src="/logo.png" height="40" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/home">Home</Nav.Link>
-            <Nav.Link as={Link} to="/sports">Sports</Nav.Link>
-          </Nav>
-          <Nav>
-          <NavDropdown title={<img className="rounded-circle" src="/images/default-user.png" alt="User Avatar" height={30} />} id="collasible-nav-dropdown">
-            <NavDropdown.Item href="">Logout</NavDropdown.Item>
-          </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+			collapseOnSelect
+			expand="lg"
+			bg="dark"
+			variant="dark"
+			sticky="top"
+		>
+			<Navbar.Brand href="/"><img src="/logo.png" height="40" /></Navbar.Brand>
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link as={Link} to="/home">Home</Nav.Link>
+					<Nav.Link as={Link} to="/sports">Sports</Nav.Link>
+					<Nav.Link as={Link} to="/subscription">Subscription</Nav.Link>
+				</Nav>
+				<Nav>
+					<NavDropdown	NavDropdown title={<img className="rounded-circle" src="/images/default-user.png" alt="User Avatar" height={30} />} id="collasible-nav-dropdown">
+					<NavDropdown.Item href="">Logout</NavDropdown.Item>
+				</NavDropdown>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
 	)
 }
